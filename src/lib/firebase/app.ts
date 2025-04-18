fa6598e9f8379275f1ae3afeb2a1943447b1772a
Jin Park <jinpk@users.noreply.github.com>
2025-04-18 21:31:46 +0900

    fix: fix firebase import error

--- src/lib/firebase/app.ts
+++ src/lib/firebase/app.ts
@@ -1,10 +1,9 @@
 import admin from 'firebase-admin'
-import { getApp, getApps } from 'firebase-admin/app'
 
 const cert = JSON.parse(process.env.FIREBASE_CERT as string)
 
-export const app = !getApps().length
+export const app = !admin.apps.length
     ? admin.initializeApp({
           credential: admin.credential.cert(cert),
       })
-    : getApp()
+    : admin.app()
